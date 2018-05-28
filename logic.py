a9685eca0316c46f479322a28afc3a7ec1124c67
Timur_hnimdvi <timurbakibayev@gmail.com>
2018-05-28 14:41:38 +0600

    changes chat_id

--- logic.py
+++ logic.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def question(text):
+def question(text, chat_id):
     if text == "Привет":
         return "Здравствуй"
     if text.upper() == "HI":
         return "Hello"
-    return "Вы сказали " + text
+    return "Вы сказали " + text + " Ваш чат айди " + str(chat_id)
--- mybot.py
+++ mybot.py
@@ -6,14 +6,14 @@ from logic import question
 # You can leave this bit out if you're using a paid PythonAnywhere account
 proxy_url = "http://proxy.server:3128"
 
-bot = telepot.Bot('587154499:AAFIxO5pErxcIdOj_r0OwhMe-ksaPURaRdo')
+bot = telepot.Bot('549769681:AAHL_1uwvmBIbGuLyaNTDJYiVVJ3PGn1YFc')
 
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(content_type, chat_type, chat_id)
 
     if content_type == 'text':
-        bot.sendMessage(chat_id, question(msg["text"]))
+        bot.sendMessage(chat_id, question(msg["text"], chat_id))
 
 bot.message_loop(handle)
 
