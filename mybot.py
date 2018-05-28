718a2ee4ff8b223b3ab7de95ca8111feba8cfe5f
Timur Bakibayev <timurbakibayev@gmail.com>
2018-05-28 14:05:21 +0600

    bot init

---
+++ mybot.py
@@ -0,0 +1,28 @@
+import telepot
+import time
+import urllib3
+
+# You can leave this bit out if you're using a paid PythonAnywhere account
+proxy_url = "http://proxy.server:3128"
+telepot.api._pools = {
+    'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
+}
+
+telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
+
+bot = telepot.Bot('587154499:AAFIxO5pErxcIdOj_r0OwhMe-ksaPURaRdo')
+
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    print(content_type, chat_type, chat_id)
+
+    if content_type == 'text':
+        bot.sendMessage(chat_id, "You said '{}'".format(msg["text"]))
+
+bot.message_loop(handle)
+
+print ('Listening ...')
+
+# Keep the program running.
+while 1:
+    time.sleep(10)
